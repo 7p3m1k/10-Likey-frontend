c5ff17b2f895436895e5c142d2e8af2e2d1ec3b9
7p3m1k <kor0885@naver.com>
2020-08-07 21:29:49 +0900

    [Likey : 박민규] Fix : 로그인 페이지 충돌해결

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,8 @@ import Main from "./Pages/Main/Main";
 import MainProduct from "./Pages/Main/MainProduct/MainProduct";
 import Detail from "./Pages/Detail/Detail";
 import SignUp from "./Pages/SignUp/SignUp";
+import SignIn from "./Components/SignIn/SignIn";
+import button from "./Components/SignIn/button";
 
 class Routes extends Component {
   render() {
@@ -14,6 +16,8 @@ class Routes extends Component {
           <Route exact path="/mainproduct" component={MainProduct} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/detail/:productId" component={Detail} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/button" component={button} />
         </Switch>
       </Router>
     );
